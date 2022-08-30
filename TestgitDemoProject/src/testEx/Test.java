de94dd165b2fc868acafe803794409caaec6df1e
Yogita Sharma <sharmayogita166@gmail.com>
2022-08-29 20:57:40 -0700

    commit Test class

---
+++ TestgitDemoProject/src/testEx/Test.java
@@ -0,0 +1,15 @@
+package testEx;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		sum();
+	}
+
+	public static int sum() {
+		int a=20;
+		int b=80;
+		return a+b;
+	}
+}
